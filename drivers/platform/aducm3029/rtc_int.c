90173c7191bcc1505c12eca7f3f99e8dab13e1cc
Andrei Drimbarean <Andrei.Drimbarean@analog.com>
2020-03-30 15:21:27 +0300

    drivers:platform:aducm3029: add RTC interrupt helper
    Add the posibility to attach a callback routine to the DFP provided
    interrupt handler for the RTC module of ADuCM3029. This implementation uses
    DFP version 3.1.2 and further.
    
    Signed-off-by: Andrei Drimbarean <Andrei.Drimbarean@analog.com>

---
+++ drivers/platform/aducm3029/rtc_int.c
@@ -0,0 +1,71 @@
+/***************************************************************************//**
+ *   @file   aducm3029/rtc_int.c
+ *   @brief  Implementation of RTC interrupt functionality for ADuCM302x
+ *   @author Andrei Drimbarean (andrei.drimbarean@analog.com)
+********************************************************************************
+ * Copyright 2020(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+
+/******************************************************************************/
+/************************* Include Files **************************************/
+/******************************************************************************/
+
+#include <stdlib.h>
+#include "rtc_int.h"
+#include "rtc_extra.h"
+#include "error.h"
+
+/******************************************************************************/
+/************************ Functions Definitions *******************************/
+/******************************************************************************/
+
+/**
+ * @brief Attach a callback routine to the DFP provided interrupt handler.
+ * @param desc - The RTC descriptor.
+ * @param cb - Pointer to the callback routine.
+ * @param cb_param - Application provided parameter for callback routine.
+ * @return SUCCESS in case of success, FAILURE otherwise.
+ */
+int32_t rtc_int_register_callback(struct rtc_dev *desc, ADI_CALLBACK cb,
+		void *cb_param)
+{
+	struct aducm_rtc_dev *adev;
+
+	if (!desc)
+		return FAILURE;
+
+	adev = desc->extra;
+
+	return adi_rtc_RegisterCallback(adev->instance, cb, cb_param);
+}
